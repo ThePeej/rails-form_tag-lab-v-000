38f005f0aa4a681476e699443e8d4c423537b93e
PJ Frias <peejfrias@gmail.com>
2018-02-19 13:31:43 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   post '/students', to: 'student#create'
   get '/students/new', to: 'students#new', as: 'new_student'
   get '/students/:id', to: 'students#show', as: 'student'
-  
+
 end
